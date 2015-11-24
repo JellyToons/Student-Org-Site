25a135f61919b58d27483272d29975dbe169d7a2
bhealy <brianhealy18@yahoo.com>
2015-11-23 17:47:03 -0800

    Commented out the allday field

--- app/models/event.js
+++ app/models/event.js
@@ -4,7 +4,7 @@ export default DS.Model.extend({
 	title: DS.attr('string'),
 	start: DS.attr('date'),
 	end: DS.attr('date'),
-	allday: DS.attr('boolean'),
+	//allday: DS.attr('boolean'),
 	tags: DS.hasMany('tag', { async: true })
 });/*.reopenClass({
   FIXTURES: [
