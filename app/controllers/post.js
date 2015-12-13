9466c5fc9cc2e030ac6d4131476c90c75055c5da
clenk <serpentine_cougar@yahoo.com>
2015-12-13 15:18:41 -0800

    Fixed Newer/Older post buttons

--- app/controllers/post.js
+++ app/controllers/post.js
@@ -6,29 +6,23 @@ export default Ember.ObjectController.extend({
   posts: (function(){
     return this.store.peekAll('post');
   }).property('posts'),
-  previousPost: Ember.computed('model', 'posts.@each', function() {
+  previousPost: Ember.computed('postContent', 'posts.@each', function() {
     var posts, index;
     posts = this.get('posts');
-    //console.log('b4 posts');
-    //console.log(posts);
-    index = posts.indexOf(this.get('model'));
+    index = posts.indexOf(this.get('postContent'));
     // Cap the index
     if (index !== -1) {index -= 1;}
+    console.log(posts.objectAt(index));
     return posts.objectAt(index);
   }),
-  nextPost: Ember.computed('model', 'posts.@each', function() {
+  nextPost: Ember.computed('postContent', 'posts.@each', function() {
     var posts, index;
     posts = this.get('posts');
-    index = posts.indexOf(this.get('model'));
+    index = posts.indexOf(this.get('postContent'));
     // Cap the index
     if (index !== -1) {index += 1;}
     return posts.objectAt(index);
   }),
-  /*previousPost: function(){
-      var index = recent.indexOf(model);
-      if (recent.objectAt(index - 1) !== undefined) { index = index -1; }
-      return recent.objectAt(index);
-  },*/
 
   topposts: Ember.computed('posts', function(){
     return this.get('posts').toArray().reverse().slice(0, 4);
