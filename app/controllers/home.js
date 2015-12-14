e17d5875c4c4d68a1dd0acb64b23cb95c6fdd347
bhealy <brianhealy18@yahoo.com>
2015-12-14 10:21:47 -0800

    Removed console logs and some commented out code

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,12 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-	/*posts: (function(){
-		return this.store.peekAll('post').toArray().reverse();
-	}).property('posts'),
-	topposts: Ember.computed('posts', function(){
-		return this.get('posts').slice(0, 4);
-	}),*/
 
 	orgNameList: (function(){
 		return this.store.peekAll('orgname');
@@ -18,8 +12,6 @@ export default Ember.Controller.extend({
 
 
 	posts: (function(){
-		console.log("getting all of this", this.orgNameList);
-		console.log("peeking At posts:", this.store.peekAll('post'));
 		return this.store.peekAll('post');
 	}).property('posts'),
 	sorting: ['id:desc'],
@@ -27,35 +19,4 @@ export default Ember.Controller.extend({
 	eventlist: (function(){
 		return this.store.peekAll('event');
 	}).property('eventlist'),
-	
-	/*aposts: null,
-	topposts: (function(){
-		var x = this.get('sortedposts');
-		console.log('stustst',x[0]);
-		this.set('aposts', this.get('sortedposts'));
-	}).observes('sortedposts').on('init'),*/
-	/*potposts: [],
-	topposts: function(){
-		console.log('HHHHEEEEEEEEEEEEE');
-		var i = 0;
-		var t = this;
-		var arr = [];
-		console.log("Sorted POSTS", this.get('sortedposts'));
-		console.log("Sorted POSTS", this.get('sortedposts').toArray());
-		this.get('sortedposts').forEach(function(post) {
-			console.log("this is a pst: ", post);
-			if (i < 4) {
-				t.get('potposts').push(post);
-			}
-			i++;
-		});
-	}.observes('sortedPosts').on('init'),*/
-	// events: (function(){
-	// 	console.log("hiihaihhh");
-	// 	console.log(this.store.peekAll('event'));
-	// 	return this.store.peekAll('event');
-	// }).property('events'),
-	/*posts: (function(){
-		return this.store.peekAll('post');
-	}).property('posts'),*/
 });
