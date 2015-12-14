c5d9f3396d0326f9e6259f22e5e9a3a3acff9aaf
bhealy <brianhealy18@yahoo.com>
2015-12-14 10:51:01 -0800

    about page content can be seen by users that are not logged in

--- app/controllers/about.js
+++ app/controllers/about.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 	orgAboutList: (function(){
+        var app = this.controllerFor('application');
+        app.set('orgabouts', this.store.findAll('orgabout'));
 		return this.store.peekAll('orgabout');
 	}).property('orgAboutList'),
 });
